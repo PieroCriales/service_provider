78aed19aa6188aaf8be7e115838b9a1a12d99925
Alexis Arias <alexis.arias555@gmail.com>
2020-12-31 01:20:14 -0500

    feat: Agregar tabla de rating
    body: Se agrego la tabla de calificaciones (ratings) por servicio
    issue: #23

---
+++ database/migrations/2020_12_31_043828_create_ratings_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ratings', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('service_id');
+            $table->unsignedBigInteger('user_id');
+            $table->integer('rating_star');
+            $table->text('rating_com');
+
+            $table->foreign('service_id')->references('id')->on('services');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ratings', function (Blueprint $table) {
+            $table->dropForeign('ratings_service_id_foreign');
+            $table->dropForeign('ratings_user_id_foreign');
+        });
+        Schema::dropIfExists('ratings');
+    }
+}
